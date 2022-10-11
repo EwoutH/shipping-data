af0f5aa2a63777c716448aeb3d6c3a484919f7ce
thomassandbergen <thomassandbergen@gmail.com>
2022-10-11 14:12:29 +0200

    Maersk expanding route details and added comments

--- Code/maersk.py
+++ Code/maersk.py
@@ -4,27 +4,35 @@ from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 import time
 
+#list of ports in south america
 list_ports_sa = ['1BX66GARX9UAH']
+#list of ports in EU
 list_ports_eu = ['1JUKNJGWHQBNJ']
 
 PATH = "chromedriver.exe"
 driver = webdriver.Chrome()
 
+#Open Maersk point to point site from SA to EU
 driver.get("https://www.maersk.com/schedules/pointToPoint?from=1BX66GARX9UAH&to=1JUKNJGWHQBNJ&containerIsoCode=42G1&fromServiceMode=CY&toServiceMode=CY&numberOfWeeks=4&dateType=D&date=2022-10-12&vesselFlag=")
 driver.implicitly_wait(2)
+#Click to allow cookies
 driver.find_element(By.XPATH,"//*[@id='coiPage-1']/div[2]/button[3]").click()
 
 driver.implicitly_wait(2)
-buttons = driver.find_elements(By.CLASS_NAME, 'button button--link show-details-link')
-print(buttons)
-for button in buttons:
-    print(1)
-    button.click()
+#This list contains all the Xpaths to the buttons that expand the route details
+list_xpath_routedetails =["//*[@id='app']/div[2]/div[1]/div[3]/div/div[4]/button/span","//*[@id='app']/div[2]/div[1]/div[4]/div/div[4]/button/span","//*[@id='app']/div[2]/div[1]/div[5]/div/div[4]/button/span",
+                          "//*[@id='app']/div[2]/div[1]/div[6]/div/div[4]/button/span","//*[@id='app']/div[2]/div[1]/div[7]/div/div[4]/button/span"]
 
+#Clicks the buttons to expand the route details
+for i in list_xpath_routedetails:
+    driver.find_element(By.XPATH,i).click()
+
+#Copy's the page to use in Beautifulsoup
 page_source = driver.page_source
 
-time.sleep(3)
-#driver.close()
+#Closes the webdriver after a few seconds
+time.sleep(5)
+driver.quit()
 
 soup = BeautifulSoup(page_source)
 
